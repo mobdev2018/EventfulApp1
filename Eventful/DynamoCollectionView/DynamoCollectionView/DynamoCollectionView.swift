e83665fa636deb38ad60e57b87a36f72a9b3ec6b
ScoreLa <scorela.team@gmail.com>
2017-10-04 12:33:42 +0700

    add dynamoCollectionView delegate/datasource interface

---
+++ Eventful/DynamoCollectionView/DynamoCollectionView/DynamoCollectionView.swift
@@ -0,0 +1,45 @@
+//
+//  DynamoCollectionView.swift
+//  DynamoCollectionView
+//
+//  Created by Thang Pham on 10/4/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+protocol DynamoCollectionViewDataSource: NSObjectProtocol {
+    func topViewRatio(_ dynamoCollectionView: DynamoCollectionView) -> Float // ratio in range [0,1]
+    func numberOfItems(_ dynamoCollectionView: DynamoCollectionView) -> Int
+}
+
+protocol DynamoCollectionViewDelegate: NSObjectProtocol {
+    func dynamoCollectionView(_ dynamoCollectionView: DynamoCollectionView, didSelectItemAt indexPath: IndexPath)
+}
+
+class DynamoCollectionView: UIView {
+    var delegate: DynamoCollectionViewDelegate?
+    var dataSource: DynamoCollectionViewDataSource?
+    private var topViewRatio: Float = 0.7
+    private var numberOfItems: Int = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func reloadData() {
+        loadView()
+    }
+    
+    private func loadView() {
+        guard let source = dataSource else { return }
+        topViewRatio = min(max(0, source.topViewRatio(self)), 1.0)
+        numberOfItems = max(source.numberOfItems(self), 0)
+    }
+}
+

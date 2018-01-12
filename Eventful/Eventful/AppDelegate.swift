d1f4cc672189b5d19805ed2aae3a2717ca8e11c9
Smiller193 <shawn.miller@temple.edu>
2018-01-12 10:52:46 -0500

    Added Crash Reporting

--- Eventful/Eventful/AppDelegate.swift
+++ Eventful/Eventful/AppDelegate.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import Firebase
-
+import Fabric
+import Crashlytics
 typealias FIRUser = FirebaseAuth.User
 
 @UIApplicationMain
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         FirebaseApp.configure()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
